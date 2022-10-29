36757598e75471048003d61bd08750871bf9e00d
Muhammad Hakim <mhmd.hakim53@gmail.com>
2022-10-29 12:37:57 +0700

    update : validation input register

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -15,15 +15,15 @@ class RegistrationController extends Controller
 
     public function store(Request $request)
     {
-        $admin_name = $request->input('admin_name');
-        $admin_email = $request->input('admin_email');
-        $admin_password = Hash::make($request->input('admin_password'));
-
-        Admin::create([
-            'admin_name' => $admin_name,
-            'admin_email' => $admin_email,
-            'admin_password' => $admin_password
+        $validated = $request->validate([
+            'admin_name' => 'required',
+            'admin_email' => 'required|email:dns|unique:admins',
+            'admin_password' => 'required',
         ]);
+        
+        $validated['admin_password'] = Hash::make($validated['admin_password']);
+
+        Admin::create($validated);
 
         return response()->json([
             'message' => "Register Sukses",
