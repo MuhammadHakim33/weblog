8b72a4579d2bbe942f0a27f756b86b4c71840e33
Muhammad Hakim <mhmd.hakim53@gmail.com>
2023-01-23 08:34:49 +0700

    install tailwind

--- resources/views/operator/partials/head.blade.php
+++ resources/views/operator/partials/head.blade.php
@@ -5,13 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"> -->
     <!-- Icon -->
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
     <!-- summernote -->
     <link rel="stylesheet" href="{{ asset('assets/summernote/summernote-lite.css') }}">
     <!-- custom -->
-    <link rel="stylesheet" href="{{ asset('assets/css/operator.css') }}">
+    <!-- <link rel="stylesheet" href="{{ asset('assets/css/operator.css') }}"> -->
+    @vite('resources/css/app.css')
     <title>{{ $title }}</title>
 </head>
 <body class="bg-light">
